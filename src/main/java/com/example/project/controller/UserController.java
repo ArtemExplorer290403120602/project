8998c4535dc4b57aa1e71d875f2bd914b490402a
User <artem.zakrzhevski@mail.ru>
2024-04-25 02:08:05 +0300

    added change service and controller and added view and added method delete USER

--- src/main/java/com/example/project/controller/UserController.java
+++ src/main/java/com/example/project/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.project.controller;
 
 import com.example.project.model.User;
+import com.example.project.repository.UserRepository;
 import com.example.project.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +19,7 @@ import java.util.Optional;
 @Controller
 public class UserController {
     private final UserService userService;
+
 
     @Autowired
     public UserController(UserService userService) {
@@ -30,11 +34,23 @@ public class UserController {
     }
 
     @GetMapping("/id")
-    public String getUserById(@PathVariable("id") Long id, ModelMap modelMap) {
+    public String getUserById(@RequestParam("id") Long id, ModelMap modelMap) {
         Optional<User> user = userService.getUserById(id);
         if (user.isPresent()) {
             modelMap.addAttribute("user", user.get());
             return "get_user";
+        }
+        return "error";
+    }
+
+    @GetMapping("/delete/id")
+    public String deleteUserById(@RequestParam("id") Long id,ModelMap modelMap){
+        Optional<User> user = userService.getUserById(id);
+        if (user.isPresent()) {
+            userService.deleteUserById(id);
+            log.info("User delete by id: " + id);
+            modelMap.addAttribute("deletedUser", user.get());
+            return "get_deleted_user";
         }
         return "error";
     }
--- src/main/java/com/example/project/service/UserService.java
+++ src/main/java/com/example/project/service/UserService.java
@@ -2,12 +2,14 @@ package com.example.project.service;
 
 import com.example.project.model.User;
 import com.example.project.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -17,11 +19,21 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userRepository.getAllUser();
     }
 
-    public Optional<User> getUserById(Long id){
+    public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
+    }
+
+    public void deleteUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.deleteById(id);
+            log.info("User delete by id: " + id);
+        } else {
+            log.error("User with ID {} not found, cannot delete", id);
+        }
     }
 }
