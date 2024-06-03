d5d6c836d8301e5fafdb4aee0e24561edd14a9a9
User <artem.zakrzhevski@mail.ru>
2024-06-04 01:26:53 +0300

    изменил таблицу

--- src/main/resources/db/migration/V1_0_4__create_table_chat.sql
+++
@@ -1,8 +0,0 @@
-create table responses (
-    id bigserial not null constraint responses_pk primary key,
-    request_id bigserial not null constraint responses_request_id_fk references requests on update cascade on delete cascade,
-    user_id bigserial not null constraint responses_user_id_fk references usr_security on update cascade on delete cascade,
-    response_text varchar(255) not null, response_date timestamp not null );
-
-alter table responses
-    add login varchar(255) not null;
